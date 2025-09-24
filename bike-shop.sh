2c42a0ab844f16a3e74e87c8312e28077312e275
Andre Pires <erdnapfaserip@gmail.com>
2025-09-24 13:51:19 -0400

    Save progress: 630.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -3,6 +3,9 @@ echo -e "\n~~~~~ Bike Rental Shop ~~~~~\n"
 
 MAIN_MENU(){
     echo "How may I help you?"
+    echo -e "\n1. Rent a bike"
+    echo "2. Return a bike"
+    echo "3. Exit"
 }
 
 MAIN_MENU
